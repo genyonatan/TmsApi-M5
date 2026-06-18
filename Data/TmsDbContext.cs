52f002067404d81186a68c3a7d3733d46dc652f2
Yonatan <yonatangenene30@gmail.com>
2026-06-18 11:04:44 +0300

    Add TMS database context

---
+++ Data/TmsDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TmsApi.Entities;
+
+namespace TmsApi.Data;
+
+public class TmsDbContext : DbContext
+{
+    public TmsDbContext(DbContextOptions<TmsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Student> Students => Set<Student>();
+
+    public DbSet<Course> Courses => Set<Course>();
+
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+}
